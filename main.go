7ffcc97b4d80743a612153c02e3a6dfce386b623
luis-octavius <luisoctavius.sc@gmail.com>
2025-11-22 16:02:59 -0300

    feat(server): add two endpoints - app and healthz

--- main.go
+++ main.go
@@ -13,11 +13,24 @@ func main () {
 		Addr: ":8080", 
 		Handler: mux,
 	}
+	
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/healthz/", http.StripPrefix("/healthz/", handlerHealthz()))
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
 
+	// ListenAndServe starts a server with an address and a handler
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal("error listening on server: %w", err)
 	}
+}
+
+func handlerHealthz() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")	
+
+		w.WriteHeader(200)
+
+		_, _ = w.Write([]byte("OK"))
+	})
 }
