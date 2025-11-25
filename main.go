dd02599e1eea2400355481effe2a22d1bf6cea66
luis-octavius <luisoctavius.sc@gmail.com>
2025-11-24 21:32:42 -0300

    feat(routes): add endpoints metric and reset
    - add apiConfig to struct to store the number of requests
    - remove StripPrefix where is not needed
    - integrate middleware in app endpoint

--- main.go
+++ main.go
@@ -1,36 +1,36 @@
-package main 
+package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
+	"sync/atomic"
 )
 
-func main () {
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
-	mux := http.NewServeMux() 
+func main() {
+
+	mux := http.NewServeMux()
+
+	var apiCfg apiConfig
 
 	server := http.Server{
-		Addr: ":8080", 
+		Addr:    ":8080",
 		Handler: mux,
 	}
-	
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	mux.Handle("/healthz/", http.StripPrefix("/healthz/", handlerHealthz()))
 
+	appHandler := http.FileServer(http.Dir("."))
+
+	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(appHandler)))
+	mux.Handle("/healthz", handlerHealthz())
+	mux.Handle("/metrics", apiCfg.handlerMetrics())
+	mux.Handle("/reset", apiCfg.handlerReset())
 
 	// ListenAndServe starts a server with an address and a handler
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal("error listening on server: %w", err)
 	}
-}
-
-func handlerHealthz() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")	
-
-		w.WriteHeader(200)
-
-		_, _ = w.Write([]byte("OK"))
-	})
 }
