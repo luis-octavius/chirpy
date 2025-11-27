9a163e09e7488c4887f154c0fafcfeed66f206dc
luis-octavius <luisoctavius.sc@gmail.com>
2025-11-26 21:04:51 -0300

    feat(database): add postgres database to handle the creation of users
    add the following things:
    - .env to hold url database
    - one goose migration to create database users
    - a query to create a user
    - postgres driver, google uuid and sqlc dependencies
    - initialize db in main and integrate in apiConfig struct
    - go.sum and .env to .gitignore

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/luis-octavius/chirpy
 
 go 1.25.1
+
+require (
+	github.com/google/uuid v1.6.0 // direct
+	github.com/lib/pq v1.10.9 // direct
+)
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,35 +1,52 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/luis-octavius/chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	queries        *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+
+	dbUrl := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbUrl)
+
+	dbQueries := database.New(db)
 
 	mux := http.NewServeMux()
 
 	var apiCfg apiConfig
+	apiCfg.queries = dbQueries
 
+	// server config
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	appHandler := http.FileServer(http.Dir("."))
-
-	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(appHandler)))
+	appHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	// endpoints
+	mux.Handle("/app/", appHandler)
 	mux.Handle("GET /api/healthz", handlerHealthz())
+
 	mux.Handle("GET /admin/metrics", apiCfg.handlerMetrics())
 	mux.Handle("POST /admin/reset", apiCfg.handlerReset())
+	mux.Handle("POST /api/validate_chirp", apiCfg.handlerValidateChirp())
 
 	// ListenAndServe starts a server with an address and a handler
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatal("error listening on server: %w", err)
 	}
